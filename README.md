a73152c24a89c9d7688c666f3517334b0dda09de
pghiraldelli <patriciaghiraldelli@gmail.com>
2016-12-17 19:00:10 -0200

    Adicionando find qd validar e adicionando mais 5 funcoes hash

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PatternMatchingBloomFilter
+
+Trabalho desenvolvido na disciplina Estrutura de Dados 2, no período 2016.2. Foi sugerida uma implementação do algoritmo de pattern matching com filtros por linha, a fim de responder rapidamente se uma determinada palavra está presente em um texto aletório e muito grande.
+
+Como rodar:
+ ```sh
+$ python patternmatching.py
+```
+
+Entradas esperadas:
+   - K: representa a quantidade de caracteres das substring utilizadas para a busca e indexação das palavras no texto.
+   - B: representa a quantidade de funções hash utilizadas na indexação de cada substring indexada (máximo de 6).
+   - M: representa o tamanho do vetor de bits que receberá 1 nas posições cujas as funções hash retornam.
+   - Palavra a ser pesquisada no texto
+
+**Lógica utilizada**
+Inicialmente o programa pré processa o texto, linha por linha, percorrendo-a de substring a substring de tamanho *K*. Para cada uma das substrings são obtidos *B* valores, referentes a cada saída de função hash. Finalmente, para cada um desses valores, a posição correspondente no vetor de bits da linha é marcada como 1 (inicialmente todas são zero).
+
+Após isso é passada uma palavra a ser buscada e realizamos as mesmas operações que o pré processamento para obter seu próprio vetor de bits.
+
+Dessa forma é possível comparar seu próprio vetor com o vetor de cada linha do texto. Caso os mesmos coincidam não há garantias de que a palavra realmente se encontra na determinada linha. Para contornar tal problema, utilizamos a função *find* do python para determinar se a palavra pertence à linha.
+
+**Referências**
+Texto utilizado:
+  - Retirado do link: http://norvig.com/big.txt
+  -	Tamanho: 6,5 MB
+  -	Salvo como *big.txt*
+
+Funções Hash utilizadas:
+- MD5
+- SHA1
+- SHA224
+- SHA256
+- SHA384
+- SHA512
--- patternmatching.py
+++ patternmatching.py
@@ -1,5 +1,6 @@
 import numpy as np
 import hashlib
+import time
 
 content = []
 allSignatures = []
@@ -16,22 +17,29 @@ def getAllSubstrings(string):
 	length = len(string)
 	return [string[i:i + k] for i in xrange(length) if (i+k) <= length]
 
-'''Returns a hash code for the substring using B hash functions.
+'''Returns a hash code for the substring using the hash function of index i.
 The result can be only smaller than B.'''
-#TODO Implementar para aceitar varias funcoes hash
-def getHashCode(substr):
-	return int(hashlib.md5(substr).hexdigest()[:8], 16) % m
+def getHashCode(substr, i):
+	return{
+		0:int(hashlib.md5(substr).hexdigest()[:8], 16) % m,
+		1:int(hashlib.sha1(substr).hexdigest()[:8], 16) % m,
+		2:int(hashlib.sha224(substr).hexdigest()[:8], 16) % m,
+		3:int(hashlib.sha256(substr).hexdigest()[:8], 16) % m,
+		4:int(hashlib.sha384(substr).hexdigest()[:8], 16) % m,
+		5:int(hashlib.sha512(substr).hexdigest()[:8], 16) % m,
+	}[i] 
 
 
-'''Returns the array of Bloom Filter signature for the line string.
+'''Returns the array of Bloom Filter signature for the string.
 The result will be of size M. Every position will be zero, except for those
 where the substring's hash is valid (in this case it will be one).'''
-def getSignatureOfLine(line):
+def getSignature(string):
 	signature = np.zeros((m,), dtype=np.int)
-	substr = getAllSubstrings(line)
+	substr = getAllSubstrings(string)
 	for sub in substr:
-		subHash = getHashCode(sub)
-		signature[subHash] = 1
+		for i in xrange(b):
+			subHash = getHashCode(sub,i)
+			signature[subHash] = 1
 	return signature
 
 '''Create the list of signatures for every line in the file.
@@ -40,29 +48,27 @@ M represents the size of the list of signature for each line.
 B represents the number of hash functions used to calculate the position in the signature list for the substring.'''
 def preProcessText():
 	for line in content:
-		signature = getSignatureOfLine(line)
+		signature = getSignature(line)
 		allSignatures.append(signature)
 
 '''Returns True if the word is in the text or False if its not.
 It divides the word in substrings of size K, get the hash of each one 
 and compare with the respective position in the line signature.
-If it matches with the signature the KMP algorithm is used to determine if
+If it matches with the signature the find method is used to determine if
 the word is really in the text.'''
 def wordInText(word):
 	subs = getAllSubstrings(word)
+	sigWord = getSignature(word)
+
 	for line in xrange(len(content)):
 		sigLine = allSignatures[line]
-		#print sigLine
-		wordInLine = True
-		for sub in subs:
-			subHash = getHashCode(sub)
-			#print sub, subHash
-			if sigLine[subHash] == 0:
-				wordInLine = False
-				break
-		if wordInLine:
-			#USAR KMP
+		
+		#get all the positions of sigLine where sigWord is 1 and then check if they're all 1.
+		sameSignature = np.all(sigLine[sigWord == 1] == 1)
+
+		if sameSignature and (word in content[line]):
 			return True
+
 	return False
 
 '''Searchs for the word in the text and check all the cases it can go wrong first.'''
@@ -76,35 +82,52 @@ def findWord(word):
 	else:
 		print '\nWord not found.'
 
+'''Verify if B is not an invalid number. Returns True if valid and False if not.'''
+def bIsValid():
+	if b == 0:
+		print 'B must be bigger than zero'
+		return False
+
+	if b > 6:
+		print 'B is bigger than the limit: 6'
+		return False
+
+	return True
+
+'''Read all the entries and verify if they all are valid. Returns True if they are and False if they are not.'''
+def readEntriesWithSuccess():
+	global k,b,m
+	try:
+		k=int(raw_input('K (size of substring to match): '))
+		b=int(raw_input('B (number of hash functions to use. LIMIT is 6): '))
+	
+		if bIsValid() == False:
+			return False
+
+		m=int(raw_input('M (size of the signature list for each line): '))
+		return True
+	except ValueError:
+		print "Not a number\n"
+		return False
 
 def main():
-	global content,k,b,m
-	#content = ['Essa e a linha 1.', 'Valeu. Essa e a linha dois.']
+	global content
 	content = readFileLines()
 
-	try:
-		k=int(raw_input('K (size of substring to match):'))
-	except ValueError:
-		print "Not a number\n"
-
-	try:
-		b=int(raw_input('B (number of hash functions to use):'))
-	except ValueError:
-		print "Not a number\n"
-
-	try:
-		m=int(raw_input('M (size of the signature list for each line):'))
-	except ValueError:
-		print "Not a number\n"
-
+	if readEntriesWithSuccess() == False:
+		return
 	
 	print '\nPreprocessing text...\n'
 	preProcessText()
 
 	while True:
-		word = raw_input('Word to search for:')
+		word = raw_input('Word to search for: ')
 
+		start_time = time.time()
 		findWord(word)
+		elapsed_time = time.time() - start_time
+		
+		print 'Total time: ' + str(elapsed_time) + '\n'
 
 
 if __name__ == "__main__": main()
